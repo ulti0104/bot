c1922360d0bbbf983f44c38213d291380411e449
Glitch (minipotato-bot-by-eoi) <none>
2024-12-24 02:42:23 +0000

    ⛪️🐣 Checkpoint ./commands/samples/schedule.mjs:72944109/4934

--- commands/samples/schedule.mjs
+++ commands/samples/schedule.mjs
@@ -1,29 +1,88 @@
-function formatDate(date, sep="") {
-  const yyyy = date.getFullYear();
-  const mm = ('00' + (date.getMonth()+1)).slice(-2);
-  const dd = ('00' + date.getDate()).slice(-2);
+const { Client, GatewayIntentBits } = require('discord.js');
 
-  return `${yyyy}${sep}${mm}${sep}${dd}`;
-}
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages] });
 
+client.login("MTMyMDkxNjk0MzE0MTczNjQ4OA.GnADJS.GzZn0riV862roNHzW-V-BdWuW_ewwfIn4XA5Sk");
 
-const dateTime = new Date();
-const date = new Date(dateTime.getFullYear(), dateTime.getMonth(), dateTime.getDate());
+client.on('ready', () => {
+    console.log(`${client.user.tag}`);
+});
 
 
-// ハイフン区切り
-const dateStrHyphen = formatDate(date, "-");
-console.log(dateStrHyphen);
-// 2022-09-14
+var tomorrow = new Date();
+tomorrow.setDate( tomorrow.getDate());
+var yearNum = tomorrow.getFullYear(); // not getYear()
+var monthNum = tomorrow.getMonth();
+var dayNum = tomorrow.getDate(); // not getDay()
+var jpDateStr = String( yearNum ) + "年" 
+                   + String( monthNum ) + "月"
+                   + String( dayNum ) + "日" ;
+
+var tomorrow = new Date();
+tomorrow.setDate( tomorrow.getDate() + 1 );
+var yearNum = tomorrow.getFullYear(); // not getYear()
+var monthNum = tomorrow.getMonth() + 1 ;
+var dayNum = tomorrow.getDate(); // not getDay()
+var jpDateStr1 = String( yearNum ) + "年" 
+                   + String( monthNum ) + "月"
+                   + String( dayNum ) + "日" ;
+
+var tomorrow = new Date();
+tomorrow.setDate( tomorrow.getDate() + 2 );
+var yearNum = tomorrow.getFullYear(); // not getYear()
+var monthNum = tomorrow.getMonth() + 2 ;
+var dayNum = tomorrow.getDate(); // not getDay()
+var jpDateStr2 = String( yearNum ) + "年" 
+                   + String( monthNum ) + "月"
+                   + String( dayNum ) + "日" ;
+
+var tomorrow = new Date();
+tomorrow.setDate( tomorrow.getDate() + 3 );
+var yearNum = tomorrow.getFullYear(); // not getYear()
+var monthNum = tomorrow.getMonth() + 3 ;
+var dayNum = tomorrow.getDate(); // not getDay()
+var jpDateStr3 = String( yearNum ) + "年" 
+                   + String( monthNum ) + "月"
+                   + String( dayNum ) + "日" ;
+
+var tomorrow = new Date();
+tomorrow.setDate( tomorrow.getDate() + 4 );
+var yearNum = tomorrow.getFullYear(); // not getYear()
+var monthNum = tomorrow.getMonth() + 4 ;
+var dayNum = tomorrow.getDate(); // not getDay()
+var jpDateStr4 = String( yearNum ) + "年" 
+                   + String( monthNum ) + "月"
+                   + String( dayNum ) + "日" ;
+
+var tomorrow = new Date();
+tomorrow.setDate( tomorrow.getDate() + 5 );
+var yearNum = tomorrow.getFullYear(); // not getYear()
+var monthNum = tomorrow.getMonth() + 5 ;
+var dayNum = tomorrow.getDate(); // not getDay()
+var jpDateStr5 = String( yearNum ) + "年" 
+                   + String( monthNum ) + "月"
+                   + String( dayNum ) + "日" ;
+
+var tomorrow = new Date();
+tomorrow.setDate( tomorrow.getDate() + 6 );
+var yearNum = tomorrow.getFullYear(); // not getYear()
+var monthNum = tomorrow.getMonth() + 6 ;
+var dayNum = tomorrow.getDate(); // not getDay()
+var jpDateStr6 = String( yearNum ) + "年" 
+                   + String( monthNum ) + "月"
+                   + String( dayNum ) + "日" ;
 
 
 
-import { SlashCommandBuilder } from 'discord.js';
 
-export const data = new SlashCommandBuilder()
-  .setName('schedule')
-  .setDescription('うるティが返事してくれるよ');
 
-export async function execute(interaction){
-  await interaction.reply(dateStrHyphen);
-}
+client.on('messageCreate', message => {
+    if (message.author.bot) return;
+    if (message.content.includes('日程入力')) {
+        message.channel.send(jpDateStr);
+        message.channel.send(jpDateStr);
+        message.channel.send(jpDateStr);
+        message.channel.send(jpDateStr);
+      
+    }
+});

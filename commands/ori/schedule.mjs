5f1f19a3458c6dbe628996a5d5fb68c3bf8e7f13
Glitch (minipotato-bot-by-eoi) <none>
2024-12-24 04:50:46 +0000

    👨‍👨‍👦😀 Checkpoint ./commands/ori/schedule.mjs:72944109/353

--- commands/ori/schedule.mjs
+++ commands/ori/schedule.mjs
@@ -4,7 +4,14 @@ export const data = new SlashCommandBuilder()
   .setName('schedule')
   .setDescription('日付入力');
 
+var now = new Date();
+
+// 本日を取得する.
+var todayString = now.getFullYear() + '年' + (now.getMonth() + 1) + '月' + now.getDate() + '日'
+var todayString1 = now.getFullYear() + '年' + (now.getMonth() + 1) + '月' + (now.getDate()+1) + '日'
+
+
 export async function execute(interaction){
-	await interaction.reply(new Date().toLocaleDateString('sv-SE'));
-  await interaction.followUp(new Date()+1.toLocaleDateString('sv-SE'));
+	await interaction.reply(todayString);
+  await interaction.followUp(todayString1);
 }

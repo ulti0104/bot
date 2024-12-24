a7dfd68fb3334fdb768ceec637c9153a9bdf267d
Glitch (minipotato-bot-by-eoi) <none>
2024-12-24 02:19:20 +0000

    🐔🚠 Checkpoint ./commands/samples/schedule.mjs:72944109/616

--- commands/samples/schedule.mjs
+++ commands/samples/schedule.mjs
@@ -1,3 +1,23 @@
+function formatDate(date, sep="") {
+  const yyyy = date.getFullYear();
+  const mm = ('00' + (date.getMonth()+1)).slice(-2);
+  const dd = ('00' + date.getDate()).slice(-2);
+
+  return `${yyyy}${sep}${mm}${sep}${dd}`;
+}
+
+
+const dateTime = new Date();
+const date = new Date(dateTime.getFullYear(), dateTime.getMonth(), dateTime.getDate());
+
+
+// ハイフン区切り
+const dateStrHyphen = formatDate(date, "-");
+console.log(dateStrHyphen);
+// 2022-09-14
+
+
+
 import { SlashCommandBuilder } from 'discord.js';
 
 export const data = new SlashCommandBuilder()
@@ -5,5 +25,5 @@ export const data = new SlashCommandBuilder()
   .setDescription('うるティが返事してくれるよ');
 
 export async function execute(interaction){
-	await interaction.reply('ありんす');
+	await interaction.reply(dateStrHyphen);
 }

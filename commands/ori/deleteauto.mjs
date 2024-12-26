ef464f50a67dd76c4c2b10eb92d74493c81e93c4
Glitch (minipotato-bot-by-eoi) <none>
2024-12-26 02:22:54 +0000

    💂🌝 Checkpoint ./commands/ori/deleteauto.mjs:72944109/577

--- commands/ori/deleteauto.mjs
+++ commands/ori/deleteauto.mjs
@@ -1,11 +1,20 @@
- client.on('messageCreate', async message => {
+import { SlashCommandBuilder } from 'discord.js';
+
+
+export const data = new SlashCommandBuilder()
+  .setName('delete')
+  .setDescription('今日の日付から入力');
+
+
+export default async(message)  => {
    // !purge コマンドが実行されたら
    if (message.content === '!purge') {
      // コマンドが送信されたチャンネルから直近100件(上限)メッセージを取得する
      const messages = await message.channel.messages.fetch({ limit: 100 })
      // ボット以外が送信したメッセージを抽出
-     const filtered = messages.filter(message => !message.author.bot)
      // それらのメッセージを一括削除
-     message.channel.bulkDelete(filtered)
+     message.channel.bulkDelete(messages)
    }
- })
+ }
+
+};

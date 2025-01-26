7dc58590a0e1b1799c10341708a034717b2499b5
Glitch (minipotato-bot-by-eoi) <none>
2025-01-26 00:42:33 +0000

    💅🍇 Checkpoint ./commands/ori/sendmessage.mjs:72944109/1579 ./commands/ori/senddm.mjs:72944109/7 ./commands/samples/Letsyamanote.mjs:72944109/64

--- commands/ori/sendmessage.mjs
+++ commands/ori/sendmessage.mjs
@@ -1,14 +1,10 @@
 import { SlashCommandBuilder, PermissionsBitField } from "discord.js";
 
 export const data = new SlashCommandBuilder()
-  .setName('sendmessage') // コマンド名
+  .setName('talk') // コマンド名
   .setDescription('管理者権限が必要') // コマンドの説明
   
-  .addUserOption(option => 
-    option.setName('user')
-      .setDescription('ユーザー指定')
-      .setRequired(true)
-  )
+
   .addStringOption(option => 
     option.setName('message')
       .setDescription('メッセージ')
@@ -29,16 +25,11 @@ export async function execute(interaction) {
   }
 
   
-  const targetUser = interaction.options.getUser('user'); // DMを送るユーザー
+  
   const messageContent = interaction.options.getString('message'); // 送信するメッセージ
 
-  // DMを送信する処理
-  try {
-    // ユーザーにDMを送信
-    await targetUser.send(messageContent);
+  
+  await interaction.channel.send(messageContent);
+  
 
-  } catch (error) {
-    console.error('DM送信エラー:', error);
-    await interaction.reply('送信失敗');
-  }
 }

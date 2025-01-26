778df3732ebfdafe09dde77740a36bcff574854b
Glitch (minipotato-bot-by-eoi) <none>
2025-01-26 00:31:02 +0000

    🎱🛎 Checkpoint ./commands/ori/senddm.mjs:72944109/617

--- commands/ori/senddm.mjs
+++ commands/ori/senddm.mjs
@@ -3,6 +3,7 @@ import { SlashCommandBuilder, PermissionsBitField } from "discord.js";
 export const data = new SlashCommandBuilder()
   .setName('sendmessage') // コマンド名
   .setDescription('権限が必要') // コマンドの説明
+  
   .addUserOption(option => 
     option.setName('user')
       .setDescription('ユーザー指定')
@@ -13,13 +14,13 @@ export const data = new SlashCommandBuilder()
       .setDescription('メッセージ')
       .setRequired(true)
                    
-                   
+             
                    
   );
 
 export async function execute(interaction) {
   
-  
+
     if (!interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
     return await interaction.reply({
       content: "このコマンドを実行する権限がありません。",

2beea894c7f73d6820347b8703276f1da27d98d9
Glitch (minipotato-bot-by-eoi) <none>
2025-04-10 18:14:16 +0000

    🏹✍️ Checkpoint ./commands/ori/koryu.mjs:72944109/231

--- commands/ori/koryu.mjs
+++ commands/ori/koryu.mjs
@@ -15,13 +15,13 @@ export const data = new SlashCommandBuilder()
 
 
 
-var now = new Date();
-now.setHours(now.getHours() + 9);
-var arr = ["(日)", "(月)", "(火)", "(水)", "(木)", "(金)" , "(土)"]
+//var now = new Date();
+//now.setHours(now.getHours() + 9);
+//var arr = ["(日)", "(月)", "(火)", "(水)", "(木)", "(金)" , "(土)"]
 // 本日を取得する.
-var youbi=now.getDay()
-var youbi=arr[youbi];
-var todayString = now.getFullYear() + '年' + (now.getMonth() + 1) + '月' + now.getDate() + '日' + youbi
+//var youbi=now.getDay()
+//var youbi=arr[youbi];
+//var todayString = now.getFullYear() + '年' + (now.getMonth() + 1) + '月' + now.getDate() + '日' + youbi
 
 
 
@@ -31,12 +31,12 @@ export async function exastringecute(interaction) {
 
   
   const aitestring = interaction.options.getString('aite'); // お相手
-  const astring = ('22:00～ \nルール \n試合数 \n試合ステージ \nランダム \nサンズ'); // 送信するメッセージ
-  const bstring = ('様 \nオーシャンズ ありんす \nルームID 1414　\n回線落ちはごめんスタンプでタスキル'); // 送信するメッセージ
-  
+  //var astring = ('22:00～ \nルール \n試合数 \n試合ステージ \nランダム \nサンズ'); // 送信するメッセージ
+  //var bstring = ('様 \nオーシャンズ ありんす \nルームID 1414　\n回線落ちはごめんスタンプでタスキル'); // 送信するメッセージ
+  const astring = ('22:00～'); // 送信するメッセージ
   
   //await interaction.channel.send('月日（）  ');
-  await interaction.channel.send(todayString + astring + aitestring + bstring);
+  await interaction.channel.send(astring);
   
 
 }

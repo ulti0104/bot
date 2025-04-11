0468abd0da4026edb7b5fdd0ef8e2fd86a56b531
Glitch (minipotato-bot-by-eoi) <none>
2025-04-11 17:33:38 +0000

    🙇😚 Checkpoint ./commands/ori/schedule3.mjs:72944109/2801

--- commands/ori/schedule3.mjs
+++ commands/ori/schedule3.mjs
@@ -24,6 +24,18 @@ export const data = new SlashCommandBuilder()
                  
 );
 
+
+
+
+
+export async function execute(interaction){
+  
+  
+  
+  
+const stday = interaction.options.getString('startday'); // 開始日
+const niti = interaction.options.getString('nisuu'); // 日数
+
 var now = new Date();
 now.setHours(now.getHours() + 9);
 
@@ -35,49 +47,19 @@ var youbi=now.getDay()
 var youbi=arr[youbi];
 var todayString = now.getFullYear() + '年' + (now.getMonth() + 1) + '月' + now.getDate() + '日' + youbi
 
-
-if 
-
-
-now.setDate(now.getDate() + 1);
+now.setDate(now.getDate() + stday);
 var youbi=now.getDay()
 var youbi=arr[youbi];
-var todayString1 = now.getFullYear() + '年' + (now.getMonth() + 1) + '月' + now.getDate() + '日' + youbi
+var todayString = now.getFullYear() + '年' + (now.getMonth() + 1) + '月' + now.getDate() + '日' + youbi
 
-now.setDate(now.getDate() + 1);
-var youbi=now.getDay()
-var youbi=arr[youbi];
-var todayString2 = now.getFullYear() + '年' + (now.getMonth() + 1) + '月' + now.getDate() + '日' + youbi
-
-now.setDate(now.getDate() + 1);
-var youbi=now.getDay()
-var youbi=arr[youbi];
-var todayString3 = now.getFullYear() + '年' + (now.getMonth() + 1) + '月' + now.getDate() + '日' + youbi
-
-now.setDate(now.getDate() + 1);var youbi=now.getDay()
-var youbi=arr[youbi];
-var todayString4 = now.getFullYear() + '年' + (now.getMonth() + 1) + '月' + now.getDate() + '日' + youbi
-
-now.setDate(now.getDate() + 1);
-var youbi=now.getDay()
-var youbi=arr[youbi];
-var todayString5 = now.getFullYear() + '年' + (now.getMonth() + 1) + '月' + now.getDate() + '日' + youbi
-
-now.setDate(now.getDate() + 1);
-var youbi=now.getDay()
-var youbi=arr[youbi];
-var todayString6 = now.getFullYear() + '年' + (now.getMonth() + 1) + '月' + now.getDate() + '日' + youbi
-
-
-
-export async function execute(interaction){
+  
+  
+  for (let i = 1; i = 3; i++){
+  console.log("i = " + i);
+}
+  
 	await interaction.channel.send(todayString);
-  await interaction.channel.send(todayString1);
-  await interaction.channel.send(todayString2);
-  await interaction.channel.send(todayString3);
-  await interaction.channel.send(todayString4);
-  await interaction.channel.send(todayString5);
-  await interaction.channel.send(todayString6);
+  
   await interaction.channel.send('@everyone \n交流戦日程⭕❌してほしいでありんす');
   await interaction.channel.send('<@1173137348968595579>');
   await interaction.channel.send('<@1108423516572881006>');

21214211e356082d91a35916ddcbd785c540d396
ulti0104 <sonoraumi0315@gmail.com>
2025-06-28 03:39:11 +0900

    Update interactionCreate.mjs

--- handlers/interactionCreate.mjs
+++ handlers/interactionCreate.mjs
@@ -1,20 +1,17 @@
-export default async(interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-	const command = interaction.client.commands.get(interaction.commandName);
+export default async (interaction) => {
+  if (!interaction.isCommand()) return;
 
-	if (!command) {
-		console.error(`「${interaction.commandName}」コマンドは見つかりませんでした。`);
-		return;
-	}
+  await interaction.deferReply(); // 
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'コマンド実行中にエラーが発生しました。', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'コマンド実行中にエラーが発生しました。', ephemeral: true });
-		}
-	}
+  const command = interaction.client.commands.get(interaction.commandName);
+  if (!command) {
+    return interaction.editReply({ content: "コマンドが見つかりませんでした。" });
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.editReply({ content: "エラーが発生しました。" });
+  }
 };
